56e7e11b2c8d7bde4a3d81a28cdcb05c0b4bf139
Carrie Hunner <carrielhunner@gmail.com>
2020-11-03 17:33:03 -0600

    cleaned up code a bit

--- REST/PhotoFrame/static/js/results.js
+++ REST/PhotoFrame/static/js/results.js
@@ -27,54 +27,54 @@ function loadIdentified() {
     let reverse = identifiedResults.reverse()
 
     $.each(reverse, (i, currentID) => {
-        const card = $('<div />').addClass('demo-card-square mdl-card mdl-shadow--2dp card');
-        const titleDiv = $('<div />').addClass('mdl-card__title mdl-card--expand');
-        const group = $('<p />').addClass('card-title mdl-card--expand bold-text').text(`Group ${currentID.groupID}`);
-        const date = $('<p />').addClass('card-title mdl-card--expand').text(`Added on ${currentID.date}`);
-        titleDiv.append(group);
-        titleDiv.append(date);
-        
-
-        const thumbnailUrl = `${currentID.mediaItems[0].baseUrl}=w256-h256`;         
-
-        const image = $('<img />').attr('src', thumbnailUrl)
-            .addClass('img-fluid rounded thumbnail');
-
-        const results = currentID.results
-        const list = $('<div />').addClass('card-ids-container')
-
-        const listTitle = $('<div />').addClass('card-id-row')
-        const name = $('<p />').addClass('card-id-common-name bold-text').text(`Possible Common Name`)
-        const score = $('<p />').addClass('card-id-score bold-text').text(`Score`)
-        listTitle.append(name)
-        listTitle.append(score)
-
-        $.each(results, (j, currentResult) => {
-          const span = $('<div />').addClass('card-id-row')
-          const name = $('<p />').addClass('card-id-common-name').text(`${j+1}. ${currentResult.commonNames[0]}`)
-          const score = $('<p />').addClass('card-id-score').text(`${currentResult.score}%`)
-          span.append(name)
-          span.append(score)
-          list.append(span)
-        })
-        
-        
-        
-        // const supportingText = $('<div />').addClass('mdl-card__supporting-text').text('Lorem ipsum dolor sit amet, consectetur adipiscing elit Aenan convallis.');
-        const border = $('<div />').addClass('mdl-card__actions mdl-card--border');
-        const button = $('<a />').addClass('mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect').text("Update");
-        border.append(button);
-
-        card.append(titleDiv);
-        card.append(image);
-        card.append(listTitle)
-        card.append(list);
-        card.append(border);
-
-        $('#results-container').append(card);
+        makeCard(currentID, i);
     });
   }
 
+function makeCard(currentID, i) {
+  const card = $('<div />').addClass('demo-card-square mdl-card mdl-shadow--2dp card');
+  const titleDiv = $('<div />').addClass('mdl-card__title mdl-card--expand');
+  const group = $('<p />').addClass('card-title mdl-card--expand bold-text').text(`Group ${currentID.groupID}`);
+  const date = $('<p />').addClass('card-title mdl-card--expand').text(`Added on ${currentID.date}`);
+  titleDiv.append(group);
+  titleDiv.append(date);
+
+  const thumbnailUrl = `${currentID.mediaItems[0].baseUrl}=w256-h256`;
+
+  const image = $('<img />').attr('src', thumbnailUrl)
+    .addClass('img-fluid rounded thumbnail');
+
+  const results = currentID.results;
+  const list = $('<div />').addClass('card-ids-container');
+
+  const listTitle = $('<div />').addClass('card-id-row');
+  const name = $('<p />').addClass('card-id-common-name bold-text').text(`Possible Common Name`);
+  const score = $('<p />').addClass('card-id-score bold-text').text(`Score`);
+  listTitle.append(name);
+  listTitle.append(score);
+
+  $.each(results, (j, currentResult) => {
+    const span = $('<div />').addClass('card-id-row');
+    const name = $('<p />').addClass('card-id-common-name').text(`${j + 1}. ${currentResult.commonNames[0]}`);
+    const score = $('<p />').addClass('card-id-score').text(`${currentResult.score}%`);
+    span.append(name);
+    span.append(toolTip);
+    span.append(score);
+    list.append(span);
+  });
+
+  const border = $('<div />').addClass('mdl-card__actions mdl-card--border');
+  const button = $('<a />').addClass('mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect').text("Update");
+  border.append(button);
+
+  card.append(titleDiv);
+  card.append(image);
+  card.append(listTitle);
+  card.append(list);
+  card.append(border);
+
+  $('#results-container').append(card);
+}
 
 $(document).ready(() => {
     loadIdentified();
